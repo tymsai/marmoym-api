d48ca55f23b66def4766aefb2e9e9353c6e6bf2d
enginehenryed <enginehenryed@gmail.com>
2017-05-07 16:56:22 +0900

    Modify models

--- src/models/index.ts
+++ src/models/index.ts
@@ -9,37 +9,31 @@ const sequelize = new Sequelize(
   config.db.password,
   {
     host: config.db.host,
-    port: config.db.port
+    port: config.db.port,
+    dialect: config.db.dialect,
+    timezone: config.db.timezone
   },
-  config.db.dialect
 );
 
-// import * as User from './user';
-
-// type Model = Sequelize.Model;
-//
-// interface DbConnection {
-//   User: Model;
-// }
-// var db = {};
-
-// var sequelize = new Sequelize(config);
-
-console.log(2);
 var User = sequelize.define('user', {
-  username: Sequelize.STRING,
-  birthday: Sequelize.DATE
-});
+    username: Sequelize.STRING,
+    birthday: Sequelize.DATE
+  }, {
+    underscored: true,
+    timestamps: true,
+    freezeTableName: true,
+  });
 
 sequelize.sync().then(function() {
   return User.create({
-    username: 'janedoe',
+    username: 'janedoe111',
     birthday: new Date(1980, 6, 20)
   });
 }).then(function(jane) {
-  console.log(jane.get({
-    plain: true
-  }));
+  console.log(1, jane)
+  // console.log(jane.get({
+  //   plain: true
+  // }));
 });
 
 export default sequelize;
