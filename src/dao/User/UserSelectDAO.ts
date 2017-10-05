ad753bdcee367f1eac97f670e42c03b601309557
mskim <minsubkim94@gmail.com>
2017-10-05 19:50:10 +0900

    Update query to not select deleted row

--- src/dao/User/UserSelectDAO.ts
+++ src/dao/User/UserSelectDAO.ts
@@ -1,8 +1,12 @@
 import db from '../../database';
+import { UserStatus } from "../../models/UserStatus";
 
 export const selectUserByEmail = (email: string) => {
   return db('User').where({
     email: email
+  })
+  .whereNot({
+    status: UserStatus.DELETED
   })
   .select();
 };
@@ -10,6 +14,9 @@ export const selectUserByEmail = (email: string) => {
 export const selectUserByUsername = (username: string) => {
   return db('User').where({
     username: username
+  })
+  .whereNot({
+    status: UserStatus.DELETED
   })
   .select();
 };
