f13020164731a8a3ffced0dc63378db6cb48c069
mskim <minsubkim94@gmail.com>
2017-10-05 20:56:58 +0900

    Update token validate service

---
+++ src/models/ErrorType/AuthError.ts
@@ -0,0 +1,12 @@
+export default {
+  'TOKEN_INVALID': {
+    type: 'TOKEN_INVALID',
+    code: 402001,
+    msg: 'Invalid token'
+  },
+  'TOKEN_AND_USER_ID_NOT_EQUAL': {
+    type: 'TOKEN_AND_USER_ID_NOT_EQUAL',
+    code: 402002,
+    msg: 'Not equal user id'
+  }
+}
