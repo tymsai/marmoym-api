4402b1be0d28880508ec1a2425c8a4ab91fb23c5
mskim <minsubkim94@gmail.com>
2017-10-06 19:03:27 +0900

    Update  to

--- src/services/errorHandleService.ts
+++ src/services/errorHandleService.ts
@@ -3,7 +3,7 @@ import * as winston from 'winston';
 export const handleError = (err, req, res, next) => {
   winston.debug(err);
   
-  if (err.constructor.name == 'MarmoymError') {
+  if (err.constructor.name === 'MarmoymError') {
     winston.warn(err);
     res.send({
       code: err.code,
