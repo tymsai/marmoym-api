0214757675c9892bccd8aef45b67b48dae577ef9
enginehenryed <enginehenryed@gmail.com>
2017-10-09 23:44:02 +0900

    Add `deploy:dev` in script of package.json
    Add get_config() in setup.sh

--- internals/pm2/pm2.config.js
+++ internals/pm2/pm2.config.js
@@ -1,9 +1,9 @@
 const serverConfig = require('../../src/config/marmoym-config').serverConfig;
 
 module.exports = {
-  "apps": [
+  apps: [
     {
-      "name": "marmoym-api",
+      name: 'marmoym-api',
       script: '/home/user/deploy/marmoym-api/current/lib/app.js',
       env: {
         COMMON_VARIABLE: 'true'
@@ -15,24 +15,24 @@ module.exports = {
   ],
   deploy: {
     development : {
-      'user': serverConfig['marmoym-dev1'].user,
-      'host': serverConfig['marmoym-dev1'].host,
-      'ref': 'origin/dev',
-      'repo': 'git@github.com:tymsai/marmoym-api.git',
-      'path': '/home/user/deploy/marmoym-api', 
-      'post-deploy': 'yarn install && yarn run reload:dev',
-      'env'  : {
+      user: serverConfig['marmoym-dev1'].user,
+      host: serverConfig['marmoym-dev1'].host,
+      ref: 'origin/dev',
+      repo: 'git@github.com:tymsai/marmoym-api.git',
+      path: '/home/user/deploy/marmoym-api', 
+      "post-deploy": 'yarn install && yarn run reload:dev',
+      env: {
         'NODE_ENV': 'development'
       }
     },
     production : {
-      user : serverConfig['marmoym-dev1'].user,
-      host : serverConfig['marmoym-dev1'].host,
-      ref  : 'origin/master',
-      repo : 'git@github.com:repo.git',
-      path : '/var/www/development',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
-      env  : {
+      user: serverConfig['marmoym-dev1'].user,
+      host: serverConfig['marmoym-dev1'].host,
+      ref: 'origin/master',
+      repo: 'git@github.com:repo.git',
+      path: '/var/www/development',
+      "post-deploy": 'npm install && pm2 reload ecosystem.config.js --env production',
+      env: {
         NODE_ENV: 'development'
       }
     }
