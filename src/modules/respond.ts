c43f95a093a790c17622f55a5d08f8da7671b14a
enginehenryed <enginehenryed@gmail.com>
2017-11-07 21:52:08 +0900

    Redefine former `respondUtils` as `respond`

---
+++ src/modules/respond.ts
@@ -0,0 +1,28 @@
+import * as winston from 'winston';
+
+import ResponseCode from '../models/ResponseCode';
+
+export default function respond(response, result, name?: string) {
+  if (result && result.then) {
+    result.then(payload => {
+      response.status(200).json({
+        code: ResponseCode.SUCCESS,
+        payload: name ?
+          { [name]: payload }
+          : { ...payload }
+      });    
+    })
+      .catch(err => {
+        winston.error(err);
+        response.status(200).json({
+          code: err.code || '000000'
+        });
+      });
+  }
+  else {
+    response.status(200).json({
+      code: 1,
+      payload: result
+    })
+  }
+};
